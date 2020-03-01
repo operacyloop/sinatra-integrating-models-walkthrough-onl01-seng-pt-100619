30cca07d58d198de397070d79da3bcfa4ec5128c
Barry Ivan Silber <age.ef.ess@gmail.com>
2020-03-01 02:41:19 +0000

    Automatically backed up by Learn

--- models/text_analyzer.rb
+++ models/text_analyzer.rb
@@ -6,6 +6,29 @@ class TextAnalyzer
   def initialize(text)
     @text = text.downcase
   end
+  
+  def piglatinizer
+    a = text.split(" ")
+    b = a.map {|word| piglatinize_word(word)}
+    b.join(" ")
+  end
+
+  def piglatinize_word(word)
+    first_letter = word[0].downcase
+    if ["a", "e", "i", "o", "u"].include?(first_letter)
+        "#{word}way"
+    else
+      consonants = []
+      consonants << word[0]
+      if ["a", "e", "i", "o", "u"].include?(word[1]) == false
+        consonants << word[1]
+        if ["a", "e", "i", "o", "u"].include?(word[2]) == false
+          consonants << word[2]
+        end
+      end
+      "#{word[consonants.length..-1] + consonants.join + "ay"}"
+    end
+  end
  
   def count_of_words
     words = text.split(" ")
